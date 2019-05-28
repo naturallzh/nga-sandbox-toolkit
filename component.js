2151c70bf29359e52329433870ad533938e3d8cb
Seraphix138 <421015792@qq.com>
2019-05-28 16:51:44 +0800

    update

---
+++ component.js
@@ -0,0 +1,31 @@
+Vue.component('countdown-v1', {
+  template:
+    '<div id="countdown-v1-box">' +
+      '<div id="title">爱因斯坦UP</div>' +
+      '<div id="time-board">' +
+        '<div class="text" style="margin-right: 15px">3天</div>' +
+        '<div class="text">1</div>' +
+        '<div class="text">9</div>' +
+        '<div class="colon">:</div>' +
+        '<div class="text">0</div>' +
+        '<div class="text">7</div>' +
+        '<div class="colon">:</div>' +
+        '<div class="text">5</div>' +
+        '<div class="text" style="margin-right: 2px">9</div>' +
+      '</div>' +
+    '</div>',
+
+  data: function () {
+    return {
+    }
+  },
+  props: {},
+  computed: {},
+  watch: {},
+  methods: {},
+
+  beforeCreate () {},
+  created () {},
+  beforeMount () {},
+  mounted (){},
+});
---
+++ index.js
@@ -0,0 +1,20 @@
+let vm = new Vue({
+  el: '#index-body',
+  data: {
+  },
+
+  beforeCreate: function () {
+  },
+  created: function () {
+  },
+  beforeMount: function (){
+  },
+  mounted: function (){
+  },
+
+  computed: {},
+  methods: {
+    test: function () {
+    }
+  }
+});
