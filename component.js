28c1191dba4139f10c27b8eae068070aeda823e3
Seraphix138 <421015792@qq.com>
2019-05-28 18:03:49 +0800

    update

--- component.js
+++ component.js
@@ -1,9 +1,10 @@
 Vue.component('countdown-v1', {
   template:
     '<div id="countdown-v1-box">' +
-      '<div id="title">爱因斯坦UP</div>' +
+      '<div id="title">{{info.title}}</div>' +
+      '<div id="end-time">至 {{endTimeStr}}</div>' +
       '<div id="time-board">' +
-        '<div class="text" style="margin-right: 15px">3天</div>' +
+        '<div class="text" style="margin-right: 15px;opacity: 0.3">00天</div>' +
         '<div class="text">1</div>' +
         '<div class="text">9</div>' +
         '<div class="colon">:</div>' +
@@ -17,15 +18,40 @@ Vue.component('countdown-v1', {
 
   data: function () {
     return {
+      endTimeStr: "",
+      countdownTime: [],
     }
   },
-  props: {},
+  props: {
+    info: {
+      type: Object,
+    },
+  },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    propsInit: function () {
+      this.endTimeStr = this.info.end_y+"年"+
+        this.addZero(this.info.end_m)+"月"+
+        this.addZero(this.info.end_d)+"日 "+
+        this.addZero(this.info.end_h)+":"+
+        this.addZero(this.info.end_mi);
+    },
+    addZero: function (num) {
+      num = parseInt(num);
+      if (num < 10) {
+        return "0" + num;
+      }
+      else {
+        return "" + num;
+      }
+    }
+  },
 
   beforeCreate () {},
-  created () {},
+  created () {
+    this.propsInit();
+  },
   beforeMount () {},
   mounted (){},
 });
